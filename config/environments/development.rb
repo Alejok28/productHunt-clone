007d3a8891f91337d15899cd1393187f7ce62893
Alejok28 <alejo_k28@hotmail.com>
2018-03-16 16:31:49 -0500

    Adds login with devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
+   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
