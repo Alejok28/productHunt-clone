2612d806de999be213ab7b434c2f8668a865fbbc
Alejok28 <alejo_k28@hotmail.com>
2018-03-17 09:46:47 -0500

    Adds gem annotate

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  url         :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Product < ApplicationRecord
 end
