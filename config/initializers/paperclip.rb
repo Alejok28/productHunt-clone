38ea119acfae5f579296fcdb11e816811ac48e53
Alejok28 <alejo_k28@hotmail.com>
2018-03-17 15:12:13 -0500

    Adds paperclip iitializer

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,2 @@
+Paperclip::Attachment.default_options[:url] = ':producthunt-clone.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
