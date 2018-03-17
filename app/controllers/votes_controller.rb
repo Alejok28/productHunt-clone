badacd11a9dffb5397de4c8f3ac36aa44a783da7
Alejok28 <alejo_k28@hotmail.com>
2018-03-17 13:30:07 -0500

    Aunthenticate user before vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,5 @@
 class VotesController < ApplicationController
+  before_action :authenticate_user!
   def create
     product = Product.find(params[:product_id])
     product.votes.create(user: current_user)
