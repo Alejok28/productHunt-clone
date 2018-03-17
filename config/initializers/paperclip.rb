68d872d944b549f0eaa0e456192c26f8c1bf7b7c
Alejok28 <alejo_k28@hotmail.com>
2018-03-17 15:21:03 -0500

    Adds region to paperclip initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 Paperclip::Attachment.default_options[:url] = ':producthunt-clone.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 'apigateway.us-east-2.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3.us-east-2.amazonaws.com'
